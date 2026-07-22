df86ccf24f24117612fe118fc39d82c0d08c1193
Z003XSJZ <basha@bytemark.co>
2026-07-22 16:46:28 +0530

    Testing demo

--- sqlfiles/XOPS-457.sql
+++ sqlfiles/XOPS-457.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 --changeset basha:XOPS-457 runOnChange:true
 
-update OrderInvoice set status = 2001, time_modified = now(), amount = 32 where reference_number = 'ABC-123';
+update OrderInvoice set status = 2001, time_modified = now(), amount = 32 where reference_number = 'ABCD-123';
 
