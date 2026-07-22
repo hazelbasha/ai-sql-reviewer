c4ec17c002d68b408c293a774c18085c4f61962c
Z003XSJZ <basha@bytemark.co>
2026-07-22 12:41:02 +0530

    Testing S3

--- sqlfiles/XOPS-458.sql
+++ sqlfiles/XOPS-458.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 --changeset basha:XOPS-458 runOnChange:true
 
-Alter OrderInvoice add column reference_number2(varchar (25));
+Alter OrderItem add column payment_id(bigint);
 
