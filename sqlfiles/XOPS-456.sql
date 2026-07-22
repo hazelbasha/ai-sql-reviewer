749414b7647ea5154c4722f9725425b423bc7a67
Z003XSJZ <basha@bytemark.co>
2026-07-22 12:12:36 +0530

    Testing multiple files

--- sqlfiles/XOPS-456.sql
+++ sqlfiles/XOPS-456.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 --changeset basha:XOPS-456 runOnChange:true
 
-Update OrderItem set price=1234,time_modfied=now();
+Update OrderItem set price=1234,name='Testing Review',time_modfied=now();
 
